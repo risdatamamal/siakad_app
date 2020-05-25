181e4a56e40ffc4090288edfd0319a65d0d094e8
risdatamamal <risda123tamam@gmail.com>
2020-05-25 21:32:10 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Siakad Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         highlightColor: Color(0xff333333),
